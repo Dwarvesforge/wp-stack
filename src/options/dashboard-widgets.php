027ae1f9492a73d797cba797e4d95c9f024a2420
Olivier Bossel <olivier.bossel@gmail.com>
2018-11-26 13:09:25 +0100

    up

--- src/options/dashboard-widgets.php
+++ src/options/dashboard-widgets.php
@@ -5,7 +5,7 @@ namespace WPS;
 // check if the acf plugin if loaded
 if (function_exists('get_field') && is_admin()) {
 	$widgets = get_field('wps_options_remove_dashboard_widgets', 'option');
-	if (@$widgets) {
+	if (@$widgets && is_array($widgets)) {
 		$widgetIds = [];
 		foreach($widgets as $widget) {
 			$widgetIds[] = $widget['wps_options_dashboard_widget'];
